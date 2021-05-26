6f74e314820066e8c39e1a0c9428f4a86be5191f
Zurab Tsilosani <zurab.tsilosani@toptal.com>
2021-05-26 20:13:15 +0400

    npm test

---
+++ exports/testmodule.js
@@ -0,0 +1,11 @@
+export const productCard = (product)=>{
+    return `<div>${product.title}</div>`
+}
+
+
+
+export class api {
+    #getAll(){
+
+    }
+}
--- script.js
+++ script.js
@@ -1,192 +1,78 @@
-class Table{
-    _header = [];
-    tableId = '';
-    constructor(id, header){
-        this.tableId = id;
-        console.log(header);
-
-        this.header = header;
-        return this;
-    }
-    addRow(data, classes) {
-        let tbody = document.querySelector(`#${this.tableId} tbody`);
-
-        let tmp = '<tr>';
-        for (let j = 0; j < data.length; j++) {
-            tmp += `<td class="${classes}">${data[j]}</td>`;
-        }
-        tmp += '</tr>';
-        tbody.innerHTML += tmp;
-
-        return this;
-    }
-    clearBody(){
-        document.querySelector(`#${this.tableId} tbody`).innerHTML = '';
-    }
-
-    generateHead() {
-        let thead = document.querySelector(`#${this.tableId} thead tr`);
-
-        let tmp = '';
-        for (let i = 0; i < this.header.length; i++) {
-            tmp += `<th>${this.header[i]}</th>`;
-        }
-
-        thead.innerHTML = tmp;
-    }
-    generateFooter(){
-        console.log('generate footer');
-    }
-
-    get header(){
-        return this._header;
-    }
-    set header(value){
-        this._header = value;
-        this.generateHead();
-        return this;
-    }
-}
-
-
-
-class StudentsList extends Table{
-    subjects = [];
-    students = [];
-    avg = [];
-    constructor(students, subjects){
-        let header = ['Name', 'Lastname'];
-        super('container', [...header, ...subjects]);
-        this.students = students;
-        this.subjects = subjects;
-
-        this.eventListeners();
-
-        this.generateTable();
-
-    }
-    eventListeners(){
-        document.getElementById('add_student_form').addEventListener('submit', this.addStudent.bind(this));
-    }
-    generateTable(){
-
-        this.generateAvg();
-        this.generateFooter();
-        for (let i = 0; i < students.length; i++) {
-            this.addRow([students[i].name, students[i].lastname, ...students[i].scores ]);
-        }
-    }
-
-    generateFooter(){
-        let tmp = '<tfoot>' +
-            '<tr>' +
-            '<td colspan="2">Avarage</td>';
-
-        for (let i = 0; i < this.avg.length; i++) {
-            tmp += `<td>${this.avg[i]}</td>`;
-        }
-
-
-        tmp += '</tr>' +
-            '</tfoot>';
-        document.querySelector('#container tfoot').innerHTML = tmp;
-    }
-
-    //generate html avarages
-     generateAvg() {
-        // calculate avarage
-        for (let i = 0; i < this.students.length; i++) { //students[i]
-            for (let j = 0; j < this.students[i].scores.length; j++) { //students[i].scores
-
-                this.avg[j] = this.avg[j] ? this.avg[j] + this.students[i].scores[j] : this.students[i].scores[j];
-            }
-
-        }
-        for (let i = 0; i < this.avg.length; i++) {
-            this.avg[i] /= this.students.length;
-        }
-
-
-    }
-
-
-    addStudent(e) {
-        e.preventDefault();
-        let fields = document.querySelectorAll('#add_student_form input');
-        let newStudent = {
-            name: '',
-            lastname: '',
-            scores: []
-        };
-        for (let i = 0; i < fields.length; i++) {
-            if (fields[i].name === 'name') {
-                newStudent.name = fields[i].value;
-            } else if (fields[i].name === 'lastname') {
-                newStudent.lastname = fields[i].value;
-            } else {
-                newStudent.scores.push(parseInt(fields[i]. value) ? parseInt(fields[i].value) : 0);
-            }
-        }
-
-        this.students.push(newStudent);
-        this.addRow(newStudent);
-        this.generateAvg();
-        return true;
-    }
-
-    addSubject (name, scores){
-        this.subjects.push(name);
-
-        this.header = [...this.header, name];
-
-        let tmp = scores.split(',');
-
-        for(let i = 0; i< tmp.length; i++ ){
-            if(this.students[i]){
-                this.students[i].scores.push(Number(tmp[i]));
-            }
-        }
-        this.clearBody();
-        this.generateTable();
-
-    }
-
-
-}
-
-
-let subjects = ['html', 'js', 'math', 'css'];
-
-let students = [
-    {
-        name: 'zura',
-        lastname: 'tsilosani',
-        scores: [85, 30, 9, 82]
-    },
-    {
-        name: 'zura2',
-        lastname: 'kk2',
-        scores: [15, 31, 29, 27]
-    },
-    {
-        name: 'xvicha',
-        lastname: 'oriode',
-        scores: [65, 34, 69, 57]
-    },
-    {
-        name: 'name',
-        lastname: 'ragacaaa',
-        scores: [35, 34, 69, 52]
-    }
+import productCard from './exports/testmodule'
+import api from './exports/testmodule'
+let images = [
+    'https://a.allegroimg.com/s512/03c8ea/8f57a72146f792e700482de7a93a/Baner-REKLAMOWY-200x100cm-gotowy-do-powieszenia',
+    'https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg',
+    'https://hackernoon.com/hn-images/1*jFyawcsqoYctkTuZg6wQ1A.jpeg',
 ];
 
-let studentsListInstance = new StudentsList(students, subjects);
 
-document.getElementById('add_subject').addEventListener('submit', function (e) {
-    e.preventDefault();
-    let name = document.querySelector('#add_subject input[name="name"]').value;
-    let scores = document.querySelector('#add_subject input[name="scores"]').value;
-    console.log(name, scores);
-    studentsListInstance.addSubject(name, scores);
-});
+class Slider {
+    imgs = [];
+    container;
+    selector;
+    activeSide = 1;
+    interval = 0;
 
+    constructor(selector, imgs = []) {
+        productCard({title: 'asd', price: 5})
+        this.imgs = imgs;
+        this.container = document.getElementById(selector);
+        this.selector = selector;
+        this.prevSlideBtn = document.getElementById('slider-prev');
+        this.nextSlideBtn = document.getElementById('slider-next');
+        console.log(this.prevSlideBtn);
+        this.prevSlideBtn.addEventListener('click', this.slideLeft.bind(this));
+        this.nextSlideBtn.addEventListener('click', this.slideRight.bind(this));
+        this.container.addEventListener('mouseenter', e => {
+            clearInterval(this.interval);
+        });
+        this.container.addEventListener('mouseleave', e => {
+            this.loop();
+        });
+        this.renderImages();
+        this.loop();
+    }
+    fetch(){
+        api.getAll().then(
+            this.render()
+        );
+    }
+
+    loop(){
+        this.interval = setInterval(this.slideRight.bind(this), 1000);
+    }
+    renderImages() {
+        let tmp = '';
+        this.imgs.forEach(el => {
+            tmp += `<div class="img">
+                        <img src="${el}" alt="slider image"  />
+                    </div>`;
+        });
+        this.container.innerHTML = tmp;
+    }
+
+    slideLeft() {
+        this.activeSide =
+            this.activeSide === 1 ?
+            this.imgs.length
+            : this.activeSide - 1;
+        let width = this.container.offsetWidth;
+        this.container.querySelector('.img').style.margin
+            = `-${this.activeSide * width}px`;
+        console.log(this.activeSide, width, this.activeSide+width);
+
+    }
+    slideRight() {
+        this.activeSide =
+            this.activeSide ===  this.imgs.length ?
+               1
+                : this.activeSide + 1;
+        let width = this.container.offsetWidth;
+        console.log(this.activeSide, width, this.activeSide+width);
+        this.container.querySelector('.img').style.margin
+            = `-${this.activeSide * width}px`;
+    }
+}
+
+new Slider('slider-container', images);
